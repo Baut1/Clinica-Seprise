7c130e2d5cbe0824ff1d265917b5fd16082d2ba4
bauti.iglesias@gmail.com <bauti.iglesias@gmail.com>
2024-12-17 18:30:34 -0300

    unused import

--- app/src/main/java/com/example/clinicaseprise/Ayuda.kt
+++ app/src/main/java/com/example/clinicaseprise/Ayuda.kt
@@ -2,7 +2,6 @@ package com.example.clinicaseprise
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
